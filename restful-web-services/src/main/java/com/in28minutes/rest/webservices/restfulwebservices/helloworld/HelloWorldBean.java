8842536221b7bb4b9107767a29287c306eb852e3
hyeongbeomkim <hyeongbeomkim@gmail.com>
2024-09-04 17:20:54 +0900

    133강 수강완료

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,10 @@
+package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class HelloWorldBean {
+    private String message;
+}
